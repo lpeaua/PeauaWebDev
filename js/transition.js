9d8c8ff72c538864b7a0e4f4551d5f0ff2db2d75
Rich <lpeaua@gmail.com>
2018-10-18 23:44:17 -0700

    commit

--- js/transition.js
+++ js/transition.js
@@ -1,7 +1,22 @@
+// transition background color
+
 var colors = ["red", "blue", "green", "yellow"],   // the color choices
       index = 0;                                     // index of the current color
-      
+	  	  
 document.getElementById("button").onclick = function() {
   document.body.style.background = colors[index];  // set the color of body to the current color
   index = (index + 1) % colors.length;             // increment index to point to the next color (if it goes beyond the length of the coices array get it back to 0 using the modulo %)
 }
+
+// transition audio files
+
+/* var a = 1;
+
+function changeAudio() {
+	var audio = document.getElementById('playAudio');
+	if (a==1) {
+		audio.src = ''
+	}
+}
+
+*/
