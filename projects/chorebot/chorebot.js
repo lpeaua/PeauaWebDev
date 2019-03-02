c66ae6c9fb81e1f196f312971803138c1f510929
Rich <lpeaua@gmail.com>
2019-03-02 15:23:09 -0800

    commit

--- projects/chorebot/chorebot.js
+++ projects/chorebot/chorebot.js
@@ -1,3 +1,5 @@
+let currentlyPlaying = true;
+
 let doorImage1 = document.querySelector('#door1');
 let doorImage2 = document.querySelector('#door2');
 let doorImage3 = document.querySelector('#door3');
@@ -57,25 +59,41 @@ const randomChoreDoorGenerator = () => {
 };
 
 doorImage1.onclick = () => {
-  if(!isClicked(doorImage1)) {
+  if(!isClicked(doorImage1) && currentlyPlaying) {
     doorImage1.src = openDoor1;
     playDoor(doorImage1);
                                        }
 };
 
 doorImage2.onclick = () => {
-  if(!isClicked(doorImage2)) {
+  if(!isClicked(doorImage2) && currentlyPlaying) {
     doorImage2.src = openDoor2;
     playDoor(doorImage2);
                                        }
 };
 
 doorImage3.onclick = () => {
- if(!isClicked(doorImage3)) {
+ if(!isClicked(doorImage3) && currentlyPlaying) {
     doorImage3.src = openDoor3;
     playDoor(doorImage3);
                                        }
 };
+
+const startRound = () => {
+  numClosedDoors = 3;
+  doorImage1.src = closedDoorPath;
+  doorImage2.src = closedDoorPath;
+  doorImage3.src = closedDoorPath;
+  startButton.innerHTML = "Good Luck!"
+  currentlyPlaying = true;
+  randomChoreDoorGenerator();
+}
+
+startButton.onclick = () => {
+  if (!currentlyPlaying) {
+    startRound()
+  }    
+}
 
 const gameOver = (status) => {
   if(status === 'win') {
@@ -85,6 +103,6 @@ const gameOver = (status) => {
   }
 };
 
-randomChoreDoorGenerator();
+startRound()
 
 
