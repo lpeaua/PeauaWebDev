9ff4eecec50b0f6451e1c961af54462b77f5699e
Rich <lpeaua@gmail.com>
2019-03-06 01:57:32 -0800

    commit

---
+++ projects/transitions/transistions.js
@@ -0,0 +1,16 @@
+let transitions = document.body.style.backgroundColor;
+let clickMe = document.querySelector('#button');
+
+const white = "white";
+const red = "red";
+const blue = "blue";
+const green = "green";
+const yellow = "yellow";
+
+let changeColor = body => {
+	if (transitions === 'white')
+};
+
+clickMe.onclick = 
+
+
