34d50b2009ed558e0697b33440645e103d2a0171
Rich <lpeaua@gmail.com>
2019-03-06 23:19:21 -0800

    commit

--- projects/transitions/transitions.js
+++ projects/transitions/transitions.js
@@ -1,5 +1,6 @@
 let transitions = document.body;
 let clickMe = document.querySelector('#button');
+let dropShadow = document.querySelector('.footer')
 let counter = 0;
 
 const increment = () => {
@@ -14,14 +15,19 @@ const colorChange = count => {
 		transitions.style.backgroundColor = "white";
 	} else if (count === 1){
 		transitions.style.backgroundColor = "red";
+        dropShadow.style.boxShadow = '';
 	} else if (count === 2){
 		transitions.style.backgroundColor = "blue";
+		dropShadow.style.boxShadow = '';
 	} else if (count === 3){
 		transitions.style.backgroundColor = "green";
+		dropShadow.style.boxShadow = '';
 	} else if (count === 4){
 		transitions.style.backgroundColor = "yellow";
+		dropShadow.style.boxShadow = '';
 	} else if (count === 5){
 		transitions.style.backgroundColor = "purple";
+		dropShadow.style.boxShadow = '';
 	} else if (count > 5) {
 		transitions.style.backgroundColor = "grey";
 	}
