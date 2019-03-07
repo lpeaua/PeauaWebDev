7d65034066f3a7caccfca4ebb16a5a457dc41973
Rich <lpeaua@gmail.com>
2019-03-06 23:26:50 -0800

    commit

--- projects/transitions/transitions.js
+++ projects/transitions/transitions.js
@@ -15,7 +15,7 @@ const colorChange = count => {
 		transitions.style.backgroundColor = "white";
 	} else if (count === 1){
 		transitions.style.backgroundColor = "red";
-        dropShadow.style.boxShadow = null;
+        dropShadow.style.removeAttribute("box-shadow");
 	} else if (count === 2){
 		transitions.style.backgroundColor = "blue";
 		dropShadow.style.boxShadow = '';
