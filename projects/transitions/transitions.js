6bc303ebd5170499cc61cb02920e4864bb9ddcee
Rich <lpeaua@gmail.com>
2019-03-06 23:00:44 -0800

    commit

--- projects/transitions/transitions.js
+++ projects/transitions/transitions.js
@@ -1,6 +1,27 @@
 let transitions = document.body;
 let clickMe = document.querySelector('#button');
+let counter = 0;
 
+const increment = () => {
+	counter ++ 1;
+	if (counter === 4) {
+		counter = 0;
+	}
+};
+
+const colorChange = count => {
+	if (count === 0){
+		transitions.style.backgroundColor = "white";
+	} else if (count === 1){
+		transitions.style.backgroundColor = "red";
+	} else if (count === 2){
+		transitions.style.backgroundColor = "blue";
+	} else if (count === 3){
+		transitions.style.backgroundColor = "green";
+	} else {
+		transitions.style.backgroundColor = "yellow";
+	}
+};
 
 clickMe.onclick = () => {
 	changeColor();
@@ -8,10 +29,5 @@ clickMe.onclick = () => {
 
 
 const changeColor = () => {
-	transitions.style.backgroundColor = "red";
-	if (transitions.style.backgroundColor === "red") {
-		transitions.style.backgroundColor = "blue";
-	} else if (transitions.style.backgroundColor === "blue") {
-		transitions.style.backgroundColor = "green";
-	}
+	colorChange(increment);
 };
